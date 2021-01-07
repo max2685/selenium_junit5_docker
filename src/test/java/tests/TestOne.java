d1c0d59e8bd3bb322f7a1654ae0cc60923766527
maksims.tolkacovs <maksims.tolkacovs@neotech.lv>
2021-01-07 23:44:00 +0200

    Fix parallelism

--- src/test/java/tests/TestOne.java
+++ src/test/java/tests/TestOne.java
@@ -2,13 +2,10 @@ package tests;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.parallel.Execution;
-import org.junit.jupiter.api.parallel.ExecutionMode;
 import watcher.TestResultLoggerExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Execution(ExecutionMode.CONCURRENT)
 @ExtendWith(TestResultLoggerExtension.class)
 public class TestOne extends DriverManager {
 
@@ -36,59 +33,27 @@ public class TestOne extends DriverManager {
         assertEquals(7, firstNumber + secondNumber, "Sum is incorrect");
     }
 
-//    @Test
-//    public void sumTest3() {
-//        driver.get("http://delfi.lv");
-//        final int firstNumber = 2;
-//        final int secondNumber = 5;
-//        assertEquals(7, firstNumber + secondNumber, "Sum is incorrect");
-//    }
+    @Test
+    public void sumTest3() {
+        driver.get("http://delfi.lv");
+        final int firstNumber = 2;
+        final int secondNumber = 5;
+        assertEquals(7, firstNumber + secondNumber, "Sum is incorrect");
+    }
 
-//    @Test
-//    public void sumTest4() {
-//        driver.get("http://delfi.lv");
-//        final int firstNumber = 2;
-//        final int secondNumber = 5;
-//        assertEquals(7, firstNumber + secondNumber, "Sum is incorrect");
-//    }
-//
-//    @Test
-//    public void sumTest5() {
-//        driver.get("http://delfi.lv");
-//        final int firstNumber = 2;
-//        final int secondNumber = 5;
-//        assertEquals(7, firstNumber + secondNumber, "Sum is incorrect");
-//    }
-//
-//    @Test
-//    public void sumTest6() {
-//        driver.get("http://delfi.lv");
-//        final int firstNumber = 2;
-//        final int secondNumber = 5;
-//        assertEquals(7, firstNumber + secondNumber, "Sum is incorrect");
-//    }
-//
-//    @Test
-//    public void sumTest7() {
-//        driver.get("http://delfi.lv");
-//        final int firstNumber = 2;
-//        final int secondNumber = 5;
-//        assertEquals(7, firstNumber + secondNumber, "Sum is incorrect");
-//    }
-//
-//    @Test
-//    public void sumTest8() {
-//        driver.get("http://delfi.lv");
-//        final int firstNumber = 2;
-//        final int secondNumber = 5;
-//        assertEquals(7, firstNumber + secondNumber, "Sum is incorrect");
-//    }
-//
-//    @Test
-//    public void sumTest9() {
-//        driver.get("http://delfi.lv");
-//        final int firstNumber = 2;
-//        final int secondNumber = 5;
-//        assertEquals(7, firstNumber + secondNumber, "Sum is incorrect");
-//    }
+    @Test
+    public void sumTest4() {
+        driver.get("http://delfi.lv");
+        final int firstNumber = 2;
+        final int secondNumber = 5;
+        assertEquals(7, firstNumber + secondNumber, "Sum is incorrect");
+    }
+
+    @Test
+    public void sumTest5() {
+        driver.get("http://delfi.lv");
+        final int firstNumber = 2;
+        final int secondNumber = 5;
+        assertEquals(7, firstNumber + secondNumber, "Sum is incorrect");
+    }
 }
--- src/test/java/tests/TestTwo.java
+++ src/test/java/tests/TestTwo.java
@@ -1,4 +1,34 @@
 package tests;
 
-public class TestTwo {
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import watcher.TestResultLoggerExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(TestResultLoggerExtension.class)
+public class TestTwo extends DriverManager{
+    @Test
+    public void sumTest6() {
+        driver.get("http://delfi.lv");
+        final int firstNumber = 2;
+        final int secondNumber = 5;
+        assertEquals(7, firstNumber + secondNumber, "Sum is incorrect");
+    }
+
+    @Test
+    public void sumTest7() {
+        driver.get("http://delfi.lv");
+        final int firstNumber = 2;
+        final int secondNumber = 5;
+        assertEquals(7, firstNumber + secondNumber, "Sum is incorrect");
+    }
+
+    @Test
+    public void sumTest8() {
+        driver.get("http://delfi.lv");
+        final int firstNumber = 2;
+        final int secondNumber = 5;
+        assertEquals(7, firstNumber + secondNumber, "Sum is incorrect");
+    }
 }
